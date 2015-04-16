63482b525a7e33f8f727be006e467a3989bc99d7
gxhenderson <gxhenderson@live.com>
2015-04-16 16:36:45 -0400

    Update displayWelcome.php

--- displayWelcome.php
+++ displayWelcome.php
@@ -1,52 +1,46 @@
- <?php
+<?php
 
 session_start();
-include "displayWelcome.php";
-//include "CRUDSprinters.php";
-include "SprinterFunctions.php";
-include "Functions.php";
-
 $hostname = "localhost";
 $username = "CIS355gxhender";
 $password = "19942367a";
 $dbname = "CIS355gxhender";
 $usertable = "sprinters";
 
-$mysqli = new mysqli($hostname, $username, $password, $dbname);
-checkConnect($mysqli); // program dies if no connection
-// ---------- if successful connection...
-if ($mysqli) {
+# ========== FUNCTIONS ========================================================
+# ---------- checkConnect -----------------------------------------------------
 
-    // ---------- c. create table, if necessary -------------------------------
-    //createTable($mysqli); 
-    // ---------- d. initialize userSelection and $_POST variables ------------
-    $userSelection = 0;
-    $firstCall = 1; // first time program is called
-    $GoToSprinters = 2; // after user clicked InsertASprinter button on list 
-    $GoToDistance = 3; // after user clicked UpdateASprinter button on list 
+function checkConnect($mysqli) {
+    if ($mysqli->connect_errno) {
+        die('Unable to connect to database [' . $mysqli->connect_error . ']');
+        exit();
+    }
+}
 
-    $_SESSION['SprinterID'] = $_POST['uid'];
-    $userlocation = $_SESSION['location'];
+function showButtons($mysqli) {
+    // display html buttons
+		echo '<div class="col-md-12">
+			  <form action="welcome.php" method="POST">'; 
+		echo '<br><br><br><br>';
+        echo '<input type="hidden" id="hid" name="hid" value="">
+            <input type="hidden" id="uid" name="uid" value="">
+			<table>
+            <tr><td><input type="submit"  name="GoToSprinters" value="Sprinters" 
+            class="btn btn-primary"></td>
+			<td><input type="submit" name="GoToDistance" value="Distance"
+			class="btn btn-primary""></td></tr></table>
+            </form></div>';
 
-    $userSelection = $firstCall; // assumes first call unless button was clicked
-    if (isset($_POST['GoToSprinters']))
-        $userSelection = $GoToSprinters;
-    if (isset($_POST['GoToDistance']))
-        $userSelection = $GoToDistance;
-
-    switch ($userSelection):
-        case $firstCall:
-            displayHTMLHead();
-            showButtons($mysqli);
-            break;
-        case $GoToSprinters:
-            displayHTMLHead();
-			header("Location: Sprinters.php");
-            break;
-        case $GoToDistance :
-            displayHTMLHead();
-            header("Location: Distance.php");
-            break;
-    endswitch;
-} // ---------- end if ---------- end main processing ----------
+        echo "<script>
+			function setHid(num)
+			{
+				document.getElementById('hid').value = num;
+		    }
+		    function setUid(num)
+			{
+				document.getElementById('uid').value = num;
+		    }
+		 </script>";
+}
 ?>
+
